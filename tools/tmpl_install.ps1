f164cd192b4e4282d54cdbc6c58c6b0e72f8bbce
Yutaka Owada <djkotori@gmail.com>
2026-06-10 09:20:44 +0900

    Bugfix template installing.

--- tools/tmpl_install.ps1
+++ tools/tmpl_install.ps1
@@ -14,22 +14,8 @@ Write-Output "Asset-Repository = $repourl"
 $assetdir = ""
 if( $repourl.StartsWith( "https://")){
 	$git = (&where.exe "git.exe")
-	if(-not $gitdir){
-		$GIT_DIR = Join-Path $env:LOCALAPPDATA "rpgcobo\tools\git"
-		$GIT_URL = "https://github.com/git-for-windows/git/releases/download/v2.50.1.windows.1/MinGit-2.50.1-64-bit.zip"
-		if( -not (Test-Path "$GIT_DIR")) {
-			$TMP_ZIP = Join-Path $env:TEMP "mingit_tmp.zip"
-			$TMP_DIR = Join-Path $env:TEMP "mingit_tmp"
-			Invoke-WebRequest $GIT_URL -OutFile $TMP_ZIP
-		    Remove-Item -Path $TMP_DIR -Recurse -Force
-			Expand-Archive -Path $TMP_ZIP -DestinationPath $TMP_DIR
-			mkdir (Join-Path $env:LOCALAPPDATA "rpgcobo\tools")
-			Move-Item -Path $TMP_DIR -Destination $GIT_DIR
-		}
-		$git = "$GIT_DIR\cmd\git.exe"
-	}
 	$assetdir = Join-Path $env:TEMP "rpgcobo-assettmp"
-    if( Test-Path "$assetdir"){
+	if( Test-Path "$assetdir"){
 		Remove-Item -Path $assetdir -Recurse -Force
 	}
 	$a = $repourl.Split(",")
@@ -48,5 +34,8 @@ $tmpl = $tmpls[0]
 foreach( $dirmap in $tmpl.dirs) {
 	$srcdir = Join-Path $assetdir $dirmap.src
 	$dstdir = Join-Path (Get-Location) $dirmap.dst
+	if (-Not (Test-Path $dstdir)) {
+		New-Item -Path $dstdir -ItemType Directory
+	}
 	Copy-Item -Path $srcdir\* -Destination $dstdir -Recurse -Force
 }
